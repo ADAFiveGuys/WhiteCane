126481c6e145508d6d866aeeb8394ee3e46cfe44
Musk <yuiop1029@naver.com>
2023-08-25 16:16:35 +0900

    enum -> String 변경

--- WhiteCane/WhiteCane/Musk/TextFieldView.swift
+++ WhiteCane/WhiteCane/Musk/TextFieldView.swift
@@ -9,18 +9,12 @@ import SwiftUI
 
 struct TextFieldView: View {
     
-    enum textFieldKind: String {
-        case name = "이름"
-        case house = "집주소"
-        case phone = "긴급연락"
-    }
-    
-    let kind: textFieldKind
+    let kind: String
     @Binding var text: String
     var body: some View {
         
         HStack {
-            Text(kind.rawValue+":")
+            Text(kind+":")
                 .font(Font.custom("SF Pro Text", size: 30).weight(.semibold))
                 .foregroundColor(.white)
                 .multilineTextAlignment(.leading)
@@ -46,6 +40,6 @@ struct TextFieldView: View {
 
 struct TextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        TextFieldView(kind: .name, text: .constant("들린매들린매들린매"))
+        TextFieldView(kind: "name", text: .constant("들린매들린매들린매"))
     }
 }
